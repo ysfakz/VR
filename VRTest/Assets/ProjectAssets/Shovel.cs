cccc0d3a67269632a74da556fba25880a4792a65
ysfakz <yusuf12pegasus@live.com>
2025-09-15 14:54:20 +0200

    add shovel sfx

--- VRTest/Assets/ProjectAssets/Shovel.cs
+++ VRTest/Assets/ProjectAssets/Shovel.cs
@@ -5,19 +5,20 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 public class Shovel : MonoBehaviour
 {
     private XRSimpleInteractable interactable;
+    [SerializeField] private AudioClip audioClip;
 
     private void Awake()
     {
         interactable = GetComponent<XRSimpleInteractable>();
         interactable.selectEntered.AddListener(OnSelect);
-
     }
 
     private void OnSelect(SelectEnterEventArgs args)
     {
-        GameManager.Instance.hasShovel = true;   // Track shovel
+        AudioSource.PlayClipAtPoint(audioClip, gameObject.transform.position, 1);
+        GameManager.Instance.hasShovel = true;
         GameManager.Instance.shovelImage.SetActive(true);
-        gameObject.SetActive(false);             // Hide shovel
+        gameObject.SetActive(false);
         Debug.Log("Shovel Grabbed");
     }
 }
